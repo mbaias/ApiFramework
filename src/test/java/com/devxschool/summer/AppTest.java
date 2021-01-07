d113f02105930c9501ebe9fb53dc238eb5ccf2f9
Baias Mametjanov <mametjanov.baias@gmail.com>
2021-01-06 19:10:10 -0600

    Update

--- src/test/java/com/devxschool/summer/AppTest.java
+++ src/test/java/com/devxschool/summer/AppTest.java
@@ -2,16 +2,24 @@ package com.devxschool.summer;
 
 import static io.restassured.RestAssured.*;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
+
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    }
+
     @Test
     public void getUserDetailsById_1() {
 
@@ -24,7 +32,7 @@ public class AppTest {
             .contentType(ContentType.JSON)
             .accept(ContentType.JSON)
         .when()
-            .get("https://jsonplaceholder.typicode.com/users/4")
+            .get("/users/4")
         .then()
             .statusCode(200)
             .body("id", Matchers.equalTo(4));
@@ -38,7 +46,7 @@ public class AppTest {
 
     @Test
     public void getUserDetailsById_2() {
-        Response response = when().get("https://jsonplaceholder.typicode.com/users/4");
+        Response response = when().get("/users/4");
 
         int id = response.body().jsonPath().getInt("id");
         String latitude = response.body().jsonPath().getString("address.geo.lat");
@@ -50,7 +58,9 @@ public class AppTest {
 
     @Test
     public void getUserDetailsById_3() {
-        Response response = when().request("GET", "https://jsonplaceholder.typicode.com/users/4");
+        Response response = given()
+            .pathParam("userId", "4")
+            .when().request("GET", "/users/{userId}");
 
         int id = response.body().jsonPath().getInt("id");
         String latitude = response.body().jsonPath().getString("address.geo.lat");
@@ -62,11 +72,14 @@ public class AppTest {
 
     @Test
     public void getAllUsers_1() {
-        Response response = when().request("GET", "https://jsonplaceholder.typicode.com/users");
+        Response response = given()
+            .queryParam("name", "Leanne Graham")
+            .when().request("GET", "/users");
 
-        int id = response.body().jsonPath().getInt("[1].id");
+        response.then().body("$", Matchers.hasSize(1));
+
+        int id = response.body().jsonPath().getInt("[0].id");
         System.out.println(id);
-        Assert.assertEquals(2, id);
     }
 
     @Test
@@ -78,13 +91,20 @@ public class AppTest {
                         "    \"username\": \"mbaias\",\n" +
                         "    \"email\": \"afnvld@gmail.com\"\n" +
                         "}")
-                .when().request("POST", "https://jsonplaceholder.typicode.com/users");
+                .when().request("POST", "/users");
 
         Assert.assertEquals(201, response.getStatusCode());
 
         String name = response.body().jsonPath().getString("name");
 
         Assert.assertEquals("baiastan", name);
+    }
 
+    @Test
+    public void deleteUser() {
+        Response response = when().request("DELETE", "/users/4");
+
+        Assert.assertEquals(200, response.getStatusCode());
+        Assert.assertEquals("{}", response.asString());
     }
 }
